a56db115510ca7d8713327ccf084bfb6a8c5b3bf
unixbox-net <104218206+unixbox-net@users.noreply.github.com>
2025-09-26 05:40:31 -0700

    Create variables.tf

---
+++ b/demo/infra/terraform/ca-central-1/variables.tf
@@ -0,0 +1,12 @@
+
+variable "region" {
+  description = "AWS region"
+  type        = string
+  default     = "ca-central-1"
+}
+
+variable "project" {
+  description = "Project name prefix"
+  type        = string
+  default     = "demo-app"
+}
